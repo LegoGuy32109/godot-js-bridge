ad96f00e94148ef456e832b8867a24601496e80d
LegoGuy32109 <joshevanhale@gmail.com>
2025-02-03 22:21:55 -0500

    use custom shell, need to work on glue code for parsing offers

--- Test.gd
+++ Test.gd
@@ -9,7 +9,30 @@ var jsJSON
 
 # must keep callback in global scope so it isn't discarded
 var generate_offers_callback = JavaScriptBridge.create_callback(_on_generate_offers)
-var generate_offers_func = JavaScriptBridge.get_interface("generateRoomConnections")
+func _on_generate_offers(args):
+	var result = args[0]
+	console.log(result)
+	print("Offers:", jsJSON.stringify(result))
+
+	navigator.clipboard.writeText(jsJSON.stringify(result))
+
+
+var parse_offer_callback = JavaScriptBridge.create_callback(_parse_offer_func)
+func _parse_offer_func(possible_offer_array):
+	if (possible_offer_array):
+		print("Possible Offer:", possible_offer_array)
+		console.log("Possible Offer:", JSON.stringify(possible_offer_array))
+		# var offer_pkg = jsJSON.parse(possible_offer)
+		# print(offer_pkg)
+
+
+var parse_answer_callback = JavaScriptBridge.create_callback(parse_answer_func)
+func parse_answer_func(possible_answer):
+	if (possible_answer):
+		var answer_pkg = jsJSON.parse(possible_answer)
+		if (answer_pkg.candidates && answer_pkg.description):
+			window.acceptAnswer(answer_pkg)
+
 
 func _ready():
 	if !OS.has_feature('web'):
@@ -23,27 +46,27 @@ func _ready():
 	navigator = JavaScriptBridge.get_interface("navigator")
 	jsJSON = JavaScriptBridge.get_interface("JSON")
 
-	window.generateRoomConnections().then(generate_offers_callback)
 
-func _on_generate_offers(args):
-	var result = args[0]
-	console.log(result)
-	print("Offers:", jsJSON.stringify(result))
-	
-	navigator.clipboard.writeText(jsJSON.stringify(result))
-
-
-
-func _on_download_button():
+func _on_generate_offers_button():
 	if !OS.has_feature('web'):
 		print("Test must be run on Web export")
 		return
 
 	window.generateRoomConnections().then(generate_offers_callback)
 
-func _on_upload_button():
+func _on_join_room_button():
 	if !OS.has_feature('web'):
 		print("Test must be run on Web export")
 		return
 
-	# window.input.click()
+	print("Attempting to parse offer package")
+	navigator.clipboard.readText().then(parse_offer_callback)
+
+func _on_accept_answer_button():
+	if !OS.has_feature('web'):
+		print("Test must be run on Web export")
+		return
+
+	print("Attempting to parse answer package")
+
+	navigator.clipboard.readText().then(parse_answer_callback)
