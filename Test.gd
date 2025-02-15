e103bc79d0e5e89be26fb9fc86faa81470252f53
LegoGuy32109 <joshevanhale@gmail.com>
2025-02-15 13:31:16 -0500

    chat works!!!!!

--- Test.gd
+++ Test.gd
@@ -10,7 +10,7 @@ var room_offer: String
 var channel
 
 # Guest data channels to with their ids (you are a host)
-var guest_channels: Array[Dictionary]
+var guest_channels = [] # Array[Dictionary]
 
 func not_on_web() -> bool:
 	if !OS.has_feature('web'):
@@ -77,14 +77,69 @@ var on_channel_open_callback = JavaScriptBridge.create_callback(_on_channel_open
 func _on_channel_open(args):
 	var data_channel = args[0]
 
-	# TODO: send message that someone joined
+	# If an id was also passed, you're the host
 	if args.size() > 1:
 		guest_channels.append({"channel": data_channel, "id": args[1]})
 	else:
 		channel = data_channel
+		channel.send("#JOIN# %s" % [display_name])
 
 	%lobby_chat_textedit.visible = true
 	%send_msg_button.visible = true
+
+
+var on_message_received_callback = JavaScriptBridge.create_callback(_on_message_received)
+func _on_message_received(args):
+	var message = args[0]
+	print(message)
+	var words = message.split(" ")
+
+	# If an id was also passed, you're the host
+	if args.size() > 1:
+		var id = args[1]
+		# The host just got a JOIN message
+		match words[0]:
+			"#JOIN#":
+				print(words)
+				var guest_name = message.substr(len("#JOIN#"))
+				print(guest_name)
+				# Update the guest with the proper name
+				guest_channels = guest_channels.map(func(guest) -> Dictionary:
+					if guest.id == id:
+						guest.name = guest_name
+					return guest
+				)
+				var new_message = ChatMessage.create("%s joined the room" % [guest_name])
+				%chat_messages.add_child(new_message)
+			"#MSG#":
+				var json = JSON.new()
+				var error = json.parse(message.substr(len("#MSG#")))
+				if error != OK || !json.data.has("name") || !json.data.has("message"):
+					printerr("bad data", message)
+					return
+
+				var new_message = ChatMessage.from(json.data.name, json.data.message)
+				%chat_messages.add_child(new_message)
+
+
+		for guest in guest_channels:
+			guest.channel.send(message)
+
+	# If you only recieved a message, you're a guest
+	else:
+		match words[0]:
+			"#JOIN#":
+				var new_message = ChatMessage.create("%s joined the room" % message.substr(len("#JOIN#")))
+				%chat_messages.add_child(new_message)
+			"#MSG#":
+				var json = JSON.new()
+				var error = json.parse(message.substr(len("#MSG#")))
+				if error != OK || !json.data.has("name") || !json.data.has("message"):
+					printerr("bad data", message)
+					return
+
+				var new_message = ChatMessage.from(json.data.name, json.data.message)
+				%chat_messages.add_child(new_message)
 
 
 func _on_generate_offers_button() -> void:
@@ -92,13 +147,13 @@ func _on_generate_offers_button() -> void:
 		return
 
 	# generate connections for each peer, host does not need one
-	window.generateRoomConnections(ROOM_SIZE - 1, on_channel_open_callback).then(offers_generated_callback)
+	window.generateRoomConnections(ROOM_SIZE - 1, on_channel_open_callback, on_message_received_callback).then(offers_generated_callback)
 
 func _on_join_room_button() -> void:
 	if not_on_web():
 		return
 
-	window.receiveConnectionOffers(on_channel_open_callback).then(offers_recieved_callback)
+	window.receiveConnectionOffers(on_channel_open_callback, on_message_received_callback).then(offers_recieved_callback)
 
 func _on_accept_answer_button() -> void:
 	if not_on_web():
@@ -139,13 +194,21 @@ func _on_send_msg_button_pressed() -> void:
 	var msg = text_edit.text
 	text_edit.clear()
 
+	# You are a guest if you only have a channel
 	if channel:
-		channel.send(msg)
+		channel.send("#MSG# %s" % [JSON.stringify({"name": display_name, "message": msg})])
+	# You are a host if you have guest channels
 	elif guest_channels:
-		for guest in guest_channels: 
-			guest.channel.send(msg)
+		for guest in guest_channels:
+			guest.channel.send("#MSG# %s" % [JSON.stringify({"name": display_name, "message": msg})])
+		# Make sure you display the message you just sent
+		var new_message = ChatMessage.from(display_name, msg)
+		%chat_messages.add_child(new_message)
 	else:
 		printerr("How did you get here?")
+		return
 
-	var new_message = ChatMessage.from("Josh", msg)
-	%chat_messages.add_child(new_message)
+
+var display_name
+func _on_display_name_text_changed() -> void:
+	display_name = %display_name_textedit.text
